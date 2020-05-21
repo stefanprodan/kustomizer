e128be4ad3d96d2fb8bef1274a1dec04417a7b4b
stefanprodan <stefan.prodan@gmail.com>
2020-05-21 14:43:24 +0300

    Apply CRDs before anything else

--- pkg/engine/applier.go
+++ pkg/engine/applier.go
@@ -5,11 +5,17 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"time"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"sigs.k8s.io/kustomize/api/filesys"
+	yaml2 "sigs.k8s.io/yaml"
 )
 
 type Applier struct {
@@ -28,19 +34,96 @@ func NewApplier(fs filesys.FileSystem, timeout time.Duration) (*Applier, error) 
 	}, nil
 }
 
-func (a *Applier) Run(filePath string, dryRun bool) error {
-	if !a.fs.Exists(filePath) {
-		return fmt.Errorf("%s not found", filePath)
+func (a *Applier) Run(manifestPath string, dryRun bool) error {
+	if !a.fs.Exists(manifestPath) {
+		return fmt.Errorf("%s not found", manifestPath)
+	}
+
+	crds, err := a.ExtractCRDs(manifestPath)
+	if err != nil {
+		return err
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), a.timeout+time.Second)
 	defer cancel()
 
-	command := fmt.Sprintf("kubectl apply -f %s --timeout=%s", filePath, a.timeout.String())
+	if crds != "" {
+		command := fmt.Sprintf("kubectl apply -f %s --timeout=%s", crds, a.timeout.String())
+		if dryRun {
+			command = fmt.Sprintf("%s --dry-run=client", command)
+		}
+
+		if err := a.exec(ctx, command); err != nil {
+			return err
+		}
+	}
+
+	command := fmt.Sprintf("kubectl apply -f %s --timeout=%s", manifestPath, a.timeout.String())
 	if dryRun {
 		command = fmt.Sprintf("%s --dry-run=client", command)
 	}
 
+	if err := a.exec(ctx, command); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (a *Applier) ExtractCRDs(manifestPath string) (string, error) {
+	manifests, err := ioutil.ReadFile(manifestPath)
+	if err != nil {
+		return "", err
+	}
+
+	crds := ""
+	reader := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(manifests), 2048)
+	for {
+		var obj unstructured.Unstructured
+		err := reader.Decode(&obj)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return "", err
+		}
+		if obj.IsList() {
+			err := obj.EachListItem(func(item runtime.Object) error {
+				return nil
+			})
+			if err != nil {
+				return "", err
+			}
+		} else {
+			if obj.GetKind() == "CustomResourceDefinition" {
+				b, err := obj.MarshalJSON()
+				if err != nil {
+					return "", err
+				}
+
+				y, err := yaml2.JSONToYAML(b)
+				if err != nil {
+					return "", err
+				}
+				crds += "---\n" + string(y)
+			}
+		}
+	}
+
+	if crds == "" {
+		return "", nil
+	}
+
+	base := filepath.Dir(manifestPath)
+	crdsFile := filepath.Join(base, "extracted-crds.yaml")
+
+	if err := ioutil.WriteFile(crdsFile, []byte(crds), os.ModePerm); err != nil {
+		return "", err
+	}
+
+	return crdsFile, nil
+}
+
+func (a *Applier) exec(ctx context.Context, command string) error {
 	var stdoutBuf, stderrBuf bytes.Buffer
 	c := exec.CommandContext(ctx, "/bin/sh", "-c", command)
 	c.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
